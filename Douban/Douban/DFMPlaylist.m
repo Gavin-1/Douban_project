3069210ad598a80f4c7e91b59c930fa095450de1
lkjy <yang@lkjydemac-mini-5.local>
2016-05-04 13:57:44 +0800

    uu

---
+++ Douban/Douban/DFMPlaylist.m
@@ -0,0 +1,19 @@
+//
+//  DFMPlaylist.m
+//  Douban
+//
+//  Created by YANG on 16/5/4.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import "DFMPlaylist.h"
+#import "SongInfo.h"
+
+@implementation DFMPlaylist
++(NSDictionary*)objectClassInArray
+{
+    return @{@"song":NSStringFromClass([SongInfo class]),};
+}
+
+
+@end
---
+++ Douban/Douban/SongInfo.m
@@ -0,0 +1,13 @@
+//
+//  SongInfo.m
+//  Douban
+//
+//  Created by YANG on 16/5/4.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import "SongInfo.h"
+
+@implementation SongInfo
+
+@end
