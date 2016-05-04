9e49c66ef92527b952b5867a86f1272e52294f8d
lk <919745602@qq.com>
2016-05-04 21:16:43 +0800

    搞好了，细节你自己搞

--- Douban/Douban/AppDelegate.h
+++ Douban/Douban/AppDelegate.h
@@ -7,11 +7,15 @@
 //我是李娟娟,wo ,55
 
 #import <UIKit/UIKit.h>
+#import "UserInfo.h"
+#import "DFMPlaylist.h"
+
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
+@property (nonatomic) UserInfo *userInfo;
 @end
 
