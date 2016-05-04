592ca92b9cf9d8843ce7a5378733c197de352ee8
lk <919745602@qq.com>
2016-05-04 18:26:55 +0800

    6东方闪电

---
+++ Douban/Douban/PlayerController.h
@@ -0,0 +1,46 @@
+//
+//  PlayerController.h
+//  Douban
+//
+//  Created by lk on 16/5/4.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MediaPlayer/MPMoviePlayerController.h>
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "AppDelegate.h"
+//#import "NetworkManager.h"
+//#import "ProtocolClass.h"
+
+@interface PlayerController : MPMoviePlayerController
+//
+//@property id<DoubanDelegate> songInfoDelegate;
+
+//FM播放列表
+//@property (nonatomic) DFMPlaylist *playList;
+
+//共享列子（实例）
++ (instancetype)sharedInstance;
+
+-(instancetype)init;
+//开始播放
+-(void)startPlay;
+
+//播放操作
+/*
+暂停；
+重启
+喜欢的歌；
+不喜欢的歌；
+删除歌曲；
+跳过歌曲；
+ */
+-(void)pauseSong;
+-(void)restartSong;
+-(void)likeSong;
+-(void)dislikesong;
+-(void)deleteSong;
+-(void)skipSong;
+@end
---
+++ Douban/Douban/PlayerController.m
@@ -0,0 +1,13 @@
+//
+//  PlayerController.m
+//  Douban
+//
+//  Created by lk on 16/5/4.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import "PlayerController.h"
+
+@implementation PlayerController
+
+@end
