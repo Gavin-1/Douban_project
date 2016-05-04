282ac3e7430efd2e5c8b9f83618deb947e8acf6a
lk <919745602@qq.com>
2016-05-04 10:37:49 +0800

    66

--- Douban/Douban/DFMRecChannelsEntity.h
+++ Douban/Douban/DFMRecChannelsEntity.h
@@ -7,8 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
+//解决循环嵌套应用
 @class ChannelInfo;
 @interface DFMRecChannelsEntity : NSObject
-//强制引用，作为属性
+//强制引用ChannelInfo，作为属性
 @property(nonatomic,strong)ChannelInfo*res;
 @end
