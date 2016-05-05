af3799ee8fc24b09d2109bd8628039bff21adb2d
LIZ <1104074113@qq.com>
2016-05-05 17:40:27 +0800

    qqqq

---
+++ Douban/Douban/ChannelsTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChannelsTableViewController.h
+//  Douban
+//
+//  Created by lkjy on 16/5/5.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ChannelsTableViewController : UITableViewController
+
+@end
